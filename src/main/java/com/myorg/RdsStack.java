2e16c86cc80376d96babcd1662bb30000effc712
Matheus Leal Gomes da Silva <matheusleal@tre-pa.jus.br>
2025-07-28 22:52:17 -0300

    Comment out RdsStack implementation

--- src/main/java/com/myorg/RdsStack.java
+++ src/main/java/com/myorg/RdsStack.java
@@ -1,31 +1,31 @@
-package com.myorg;
-
-import software.amazon.awscdk.CfnParameter;
-import software.amazon.awscdk.Stack;
-import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.services.ec2.Vpc;
-import software.amazon.awscdk.services.ecs.Cluster;
-import software.amazon.awscdk.services.ecs.ContainerImage;
-import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
-import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
-import software.constructs.Construct;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
-
-public class RdsStack extends Stack {
-    public RdsStack(final Construct scope, final String id, final Vpc vpc) {
-        this(scope, id, null, vpc);
-    }
-
-    public RdsStack(final Construct scope, final String id, final StackProps props, final Vpc vpc) {
-        super(scope, id, props);
-
-        CfnParameter passwd = CfnParameter.Builder.create(this, "senha")
-                .type("String")
-                .description("Senha do banco de dados")
-                .build();
-
-
-
-    }
-}
+//package com.myorg;
+//
+//import software.amazon.awscdk.CfnParameter;
+//import software.amazon.awscdk.Stack;
+//import software.amazon.awscdk.StackProps;
+//import software.amazon.awscdk.services.ec2.Vpc;
+//import software.amazon.awscdk.services.ecs.Cluster;
+//import software.amazon.awscdk.services.ecs.ContainerImage;
+//import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+//import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+//import software.constructs.Construct;
+//// import software.amazon.awscdk.Duration;
+//// import software.amazon.awscdk.services.sqs.Queue;
+//
+//public class RdsStack extends Stack {
+//    public RdsStack(final Construct scope, final String id, final Vpc vpc) {
+//        this(scope, id, null, vpc);
+//    }
+//
+//    public RdsStack(final Construct scope, final String id, final StackProps props, final Vpc vpc) {
+//        super(scope, id, props);
+//
+//        CfnParameter passwd = CfnParameter.Builder.create(this, "senha")
+//                .type("String")
+//                .description("Senha do banco de dados")
+//                .build();
+//
+//
+//
+//    }
+//}
